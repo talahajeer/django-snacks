390897e681f87f077093db4bb89c5c608b0d438b
talahajeer <tala.hajeer@gmail.com>
2021-06-13 18:40:07 +0300

    lab26

---
+++ snacks/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+from .views import HomePageView, AboutView
+
+urlpatterns=[
+    path('', HomePageView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+]
